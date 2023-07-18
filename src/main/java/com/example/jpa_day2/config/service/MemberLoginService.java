ec750198529f295877ada34da403b5686a276278
HeeHeeg <qmffndoden@naver.com>
2023-07-18 11:15:39 +0900

    중간 완성

--- src/main/java/com/example/jpa_day2/config/service/MemberLoginService.java
+++ src/main/java/com/example/jpa_day2/config/service/MemberLoginService.java
@@ -1,13 +1,24 @@
 package com.example.jpa_day2.config.service;
 
+import com.example.jpa_day2.config.domain.entity.MemberLogin;
 import com.example.jpa_day2.config.repository.MemberLoginRepository;
+import com.example.jpa_day2.members.domain.entity.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
 public class MemberLoginService {
     private final MemberLoginRepository memberLoginRepository;
+
     // TODO: INSERT
+    public void insert(Member member) {
+        MemberLogin memberLogin = new MemberLogin(member, LocalDateTime.now());
+        memberLoginRepository.save(memberLogin);
+    }
+
+
     //  login check 하는 것 만들거임. findByMember 로 찾아줘~ 하면 된다.
 }
--- src/main/java/com/example/jpa_day2/members/domain/service/MemberService.java
+++ src/main/java/com/example/jpa_day2/members/domain/service/MemberService.java
@@ -1,6 +1,8 @@
 package com.example.jpa_day2.members.domain.service;
 
 import com.example.jpa_day2.config.domain.entity.MemberLogin;
+import com.example.jpa_day2.config.repository.MemberLoginRepository;
+import com.example.jpa_day2.config.service.MemberLoginService;
 import com.example.jpa_day2.members.domain.entity.Member;
 import com.example.jpa_day2.members.domain.repository.MemberRepository;
 import com.example.jpa_day2.members.domain.request.LoginRequest;
@@ -16,6 +18,7 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class MemberService {
     private final MemberRepository memberRepository;
+    private final MemberLoginService memberLoginService;
 
     //여기서 insert는 signup이다.
     public void insert(SignupRequest request) { // 아무것도 반환을 안해주니 그냥 insert로 적어줌.
@@ -30,7 +33,8 @@ public class MemberService {
         //있으면 꺼내고 없으면 에러를 던져주라고 설정.
         Member member =
                 byEmailAndPassword.orElseThrow(() -> new RuntimeException("없는 유저"));// -> 이제 멤버가 꺼내진거다. 여기까지 하면 로그인이 되는 것.
-        new MemberLogin(member, LocalDateTime.now());
+//        new MemberLogin(member, LocalDateTime.now()); 여기다 하지 말고 공유 폴더로 만들어서 사용 ->cofig -memberLogin을 따로 만듬.
+        memberLoginService.insert(member);
         //로그인 하고 부터는 password가 더이상 필요 없다. 확실하게 빼줄거는 거르고 빼줘야 한다. 이제  response를 만들어보자.
         return new LoginResponse(member.getId(), member.getName(), member.getAge());
 
